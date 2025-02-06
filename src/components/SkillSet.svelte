2d6960cf2e2730e6f669f791e207c66a1b5d7583
Ademir José Ferreira Júnior <ademirj.ferreirajunior@gmail.com>
2025-02-05 23:39:08 -0300

    refactor: simplify SkillSet component

--- src/components/SkillSet.svelte
+++ src/components/SkillSet.svelte
@@ -1,7 +1,6 @@
 <script lang="ts">
   import { TECH_META, type Tech } from "@app/constants";
   import Icon from "@components/Icon.svelte";
-  import Tag from "@components/Tag.svelte";
   import type { ClassValue } from "svelte/elements";
 
   interface Props {
@@ -13,21 +12,28 @@
   const { class: className, name, set }: Props = $props();
 </script>
 
+{#snippet tag(tagName: string, closing: "open" | "close")}
+  <span class="text-silver/50" aria-hidden="true">
+    &lt;{closing === "close" ? "/" : ""}{tagName}&gt;
+  </span>
+{/snippet}
+
 <article class={["typo-code flex flex-col", className]}>
-  <Tag name="h3">
-    <h3 class="pl-6 text-lg">{name}</h3>
-  </Tag>
-  <Tag name="ul">
-    <ul>
-      {#each set as tech (tech)}
-        {@const { label, color } = TECH_META[tech]}
-        <li class="flex items-center gap-1 pl-6">
-          <Tag name="li">
-            <Icon class="size-5" name={tech} {color} />
-            <span>{label}</span>
-          </Tag>
-        </li>
-      {/each}
-    </ul>
-  </Tag>
+  {@render tag("h3", "open")}
+  <h3 class="pl-6 text-lg">{name}</h3>
+  {@render tag("h3", "close")}
+
+  {@render tag("ul", "open")}
+  <ul>
+    {#each set as tech (tech)}
+      {@const { label, color } = TECH_META[tech]}
+      <li class="flex items-center gap-1 pl-6">
+        {@render tag("li", "open")}
+        <Icon class="size-5" name={tech} {color} />
+        <span>{label}</span>
+        {@render tag("li", "close")}
+      </li>
+    {/each}
+  </ul>
+  {@render tag("ul", "close")}
 </article>
