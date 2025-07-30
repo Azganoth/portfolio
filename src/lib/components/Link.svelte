b342db670cf5d96bcd8a948421b3e40ce1557f89
Ademir José Ferreira Júnior <ademirj.ferreirajunior@gmail.com>
2025-07-30 01:42:45 -0300

    refactor: remove unused link variant

--- src/lib/components/Link.svelte
+++ src/lib/components/Link.svelte
@@ -2,7 +2,7 @@
   import type { HTMLAnchorAttributes } from "svelte/elements";
 
   interface Props extends HTMLAnchorAttributes {
-    variant?: "default" | "prime" | "none";
+    variant?: "default" | "none";
     newTab?: boolean;
   }
 
@@ -19,8 +19,6 @@
   class={[
     "font-bold transition-all",
     variant === "default" && "font-inter hover:text-teal",
-    variant === "prime" &&
-      "after:-z-1 font-inter after:bg-purple relative z-0 inline-block after:absolute after:-inset-x-0.5 after:bottom-[0.1875em] after:h-[1.125rem] after:origin-bottom after:scale-y-[33.33%] after:duration-300 after:ease-out after:content-[''] hover:after:scale-100 motion-safe:after:transition-all",
     className,
   ]}
   {...newTab && { target: "_blank" }}
