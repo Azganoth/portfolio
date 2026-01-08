a44beefd1b4b2803bb646627781f1f5c702eec8e
Ademir José Ferreira Júnior <ademirfel.ferreira@gmail.com>
2026-01-07 23:18:39 -0300

    refactor: migrate js media query to css and adopt Popover API

--- src/lib/shared/components/Header.svelte
+++ src/lib/shared/components/Header.svelte
@@ -1,21 +1,17 @@
 <script lang="ts">
+  import { page } from "$app/state";
   import { t } from "$lib/features/i18n/translation.svelte";
-  import { clickaway } from "$lib/shared/attachments/clickaway.svelte";
   import Link from "$lib/shared/components/Link.svelte";
   import Logo from "$lib/shared/components/Logo.svelte";
 
   import {
     ID_BIO_SECTION,
     ID_CONTACT_SECTION,
-    ID_MOBILE_MENU,
     ID_PROJECTS_SECTION,
     ID_SKILLS_SECTION,
   } from "$lib/shared/constants";
   import { getLocalizedPath } from "$lib/shared/utils";
   import Icon from "@iconify/svelte";
-  import { MediaQuery } from "svelte/reactivity";
-
-  const mobile = new MediaQuery("not all and (min-width: 768px)");
 
   interface JumpLink {
     label: string;
@@ -46,75 +42,96 @@
     },
   ]);
 
-  let open = $state(false);
-  let menuToggler = $state<HTMLButtonElement>();
+  let mobilePopover = $state<HTMLDivElement>();
 
-  const handleKeydown = (event: KeyboardEvent) => {
-    if (open && event.key === "Escape") {
-      open = false;
+  $effect(() => {
+    if (page.url) {
+      mobilePopover?.hidePopover();
     }
-  };
+  });
 </script>
-
-<svelte:window onkeydown={handleKeydown} />
 
 <header>
   <!-- Navigation Bar -->
   <div class="fixed inset-x-0 top-4 z-50 flex justify-center">
+    <!-- Desktop Bar -->
     <div
-      class="flex items-center gap-6 rounded-full border border-white/10 bg-muted/80 p-2 px-6 shadow-elevation backdrop-blur-md"
+      class="flex items-center gap-4 rounded-full border border-white/10 bg-muted/80 p-2 px-6 shadow-elevation backdrop-blur-md max-md:hidden"
     >
+      <nav aria-label={t("a11y_desktop_navigation")}>
+        <ul class="flex items-center gap-6">
+          <li>
+            {@render homeLink()}
+          </li>
+          {#each links as { label, link, description } (link)}
+            <li>
+              <Link
+                class="font-display tracking-wide hover:text-primary"
+                variant="none"
+                href={link}
+                aria-label={description}
+              >
+                {label}
+              </Link>
+            </li>
+          {/each}
+        </ul>
+      </nav>
+      <div class="h-6 w-px bg-white/10"></div>
       <Link
+        class="-mr-3.5 flex items-center gap-2 rounded-full border border-secondary/20 bg-secondary/10 px-3 py-1 font-mono font-bold tracking-wide text-secondary transition-all ease-snappy hover:scale-105 hover:border-secondary/40 hover:bg-secondary/20 active:scale-95"
         variant="none"
-        href={getLocalizedPath("/")}
-        aria-label={t("a11y_go_to_home")}
+        href="/cv.pdf"
+        newTab
+        aria-label={t("a11y_download_cv")}
       >
-        <Logo class="h-8 w-auto" />
+        <span>{t("start_curriculum")}</span>
+        <Icon icon="fa6-solid:arrow-up-right-from-square" />
       </Link>
+    </div>
 
-      {#if mobile.current}
-        <!-- Mobile Bar -->
-        <div class="h-6 w-px bg-white/10"></div>
-        <button
-          bind:this={menuToggler}
-          class="block transition-colors hover:text-primary"
-          type="button"
-          onclick={() => {
-            open = !open;
-          }}
-          aria-label={open ? t("a11y_close_nav_menu") : t("a11y_open_nav_menu")}
-          aria-expanded={open}
-          aria-controls={ID_MOBILE_MENU}
-        >
-          <Icon
-            class="pointer-events-none size-6"
-            icon={open ? "fa6-solid:xmark" : "fa6-solid:bars"}
-          />
-        </button>
-      {:else}
-        <!-- Desktop Bar -->
-        <nav
-          aria-label={t("a11y_main_navigation")}
-          class="flex items-center gap-6"
-        >
-          <ul class="flex gap-6">
-            {#each links as { label, link, description } (link)}
-              <li>
-                <Link
-                  class="font-display tracking-wide hover:text-primary"
-                  variant="none"
-                  href={link}
-                  aria-label={description}
-                >
-                  {label}
-                </Link>
-              </li>
-            {/each}
-          </ul>
-          <div class="h-6 w-px bg-white/10"></div>
+    <!-- Mobile Bar -->
+    <div
+      class="flex items-center gap-4 rounded-full bg-background/80 p-2.5 px-20 backdrop-blur-md md:hidden"
+    >
+      {@render homeLink()}
+      <button
+        type="button"
+        class="fixed right-4 block transition-colors hover:text-primary"
+        popovertarget="mobile-menu"
+      >
+        <Icon icon="fa7-solid:bars" class="size-8" />
+      </button>
+
+      <!-- Mobile Menu -->
+      <div
+        bind:this={mobilePopover}
+        id="mobile-menu"
+        class="w-full bg-muted p-8 pt-8 transition-all transition-discrete not-open:-translate-y-16 not-open:opacity-0 starting:-translate-y-16 starting:opacity-0"
+        popover
+      >
+        <div class="flex flex-col items-center gap-6">
+          {@render homeLink()}
+          <nav>
+            <ul class="flex flex-col gap-6">
+              {#each links as { label, link, description } (link)}
+                <li>
+                  <Link
+                    class="block text-center font-display text-lg tracking-wide"
+                    href={link}
+                    aria-label={description}
+                  >
+                    {label}
+                  </Link>
+                </li>
+              {/each}
+            </ul>
+          </nav>
+
+          <div class="h-px w-full bg-white/10"></div>
+
           <Link
-            class="-mr-3.5 flex items-center gap-2 rounded-full border border-secondary/20 bg-secondary/10 px-3 py-1 font-mono font-bold tracking-wide text-secondary transition-all ease-snappy hover:scale-105 hover:border-secondary/40 hover:bg-secondary/20 active:scale-95"
-            variant="none"
+            class="flex items-center justify-center gap-2 font-mono text-lg font-bold text-primary"
             href="/cv.pdf"
             newTab
             aria-label={t("a11y_download_cv")}
@@ -122,53 +139,18 @@
             <span>{t("start_curriculum")}</span>
             <Icon icon="fa6-solid:arrow-up-right-from-square" />
           </Link>
-        </nav>
-      {/if}
+        </div>
+      </div>
     </div>
   </div>
-
-  {#if mobile.current}
-    <!-- Mobile Menu -->
-    <ul
-      id={ID_MOBILE_MENU}
-      class="fixed inset-x-0 top-0 z-40 flex origin-top flex-col gap-4 bg-muted px-8 pt-28 pb-8 shadow-elevation ease-out-expo inert:invisible inert:-translate-y-4 inert:opacity-0 motion-safe:transition-all"
-      inert={!open}
-      onclickaway={() => {
-        open = false;
-      }}
-      {@attach clickaway({
-        ignoreNodes: [menuToggler].filter(Boolean) as HTMLElement[],
-      })}
-    >
-      {#each links as { label, link, description } (link)}
-        <li>
-          <Link
-            class="block text-center font-display text-lg tracking-wide"
-            href={link}
-            onclick={() => {
-              open = false;
-            }}
-            aria-label={description}
-          >
-            {label}
-          </Link>
-        </li>
-      {/each}
-
-      <li class="border-t border-white/10 pt-4">
-        <Link
-          class="flex items-center justify-center gap-2 font-mono text-lg font-bold text-primary"
-          href="/cv.pdf"
-          newTab
-          onclick={() => {
-            open = false;
-          }}
-          aria-label={t("a11y_download_cv")}
-        >
-          <span>{t("start_curriculum")}</span>
-          <Icon icon="fa6-solid:arrow-up-right-from-square" />
-        </Link>
-      </li>
-    </ul>
-  {/if}
 </header>
+
+{#snippet homeLink()}
+  <Link
+    variant="none"
+    href={getLocalizedPath("/")}
+    aria-label={t("a11y_go_to_home")}
+  >
+    <Logo class="h-8 w-auto" />
+  </Link>
+{/snippet}
