117d89fca36255fed3b04ea7a1ae8fde473c4f54
Ademir José Ferreira Júnior <ademirfel.ferreira@gmail.com>
2026-04-16 20:27:24 -0300

    chore: drop eslint-plugin-only-warn

--- eslint.config.js
+++ eslint.config.js
@@ -2,7 +2,6 @@
 import { includeIgnoreFile } from "@eslint/compat";
 import js from "@eslint/js";
 import prettier from "eslint-config-prettier";
-import onlyWarn from "eslint-plugin-only-warn";
 import svelte from "eslint-plugin-svelte";
 import { defineConfig } from "eslint/config";
 import globals from "globals";
@@ -38,12 +37,6 @@ export default defineConfig(
     rules: {
       // Resolve gets called in the getLocalizedPath utility
       "svelte/no-navigation-without-resolve": "off",
-    },
-  },
-  {
-    plugins: {
-      // @ts-expect-error 'eslint-plugin-only-warn' does not provide type definitions
-      onlyWarn,
     },
   },
   prettier,
